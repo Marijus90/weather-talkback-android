ea468b182212adb4458519009b6667b711fbcd5a
Marijus90 <marijus.jalinskas@gmail.com>
2018-10-02 17:09:59 +0100

    Added LeakCanary to detect any memory leaks

--- app/src/main/java/accessibility/forecast/marijus/weathertalkback2/base/WeatherApp.java
+++ app/src/main/java/accessibility/forecast/marijus/weathertalkback2/base/WeatherApp.java
@@ -2,6 +2,8 @@ package accessibility.forecast.marijus.weathertalkback2.base;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import accessibility.forecast.marijus.weathertalkback2.helper.di.components.AppComponent;
 import accessibility.forecast.marijus.weathertalkback2.helper.di.components.DaggerAppComponent;
 import dagger.android.AndroidInjector;
@@ -13,6 +15,15 @@ import dagger.android.DaggerApplication;
  * We never have to call `component.inject(this)` as {@link DaggerApplication} will do that for us.
  */
 public class WeatherApp extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+    }
 
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
